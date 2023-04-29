37ddf55a0e0637de77867f5b6bb7b66dfdd63478
sjesus <s.jesus10@yahoo.com>
2023-04-29 14:19:02 -0500

    Fourth Commit

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,16 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static  HashMap<String, String> events = new HashMap();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        events.put("Name of Event One", "Description of Event One");
+        events.put("Name of Event Two", "Description of Event Two");
+        events.put("Name of Event Three", "Description of Event Three");
        model.addAttribute("events", events);
         return "events/index";
     }
@@ -30,8 +34,8 @@ public class EventController {
 
 //    lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:";
     }
 }
